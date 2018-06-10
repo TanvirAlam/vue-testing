9fb6181ff0f0a5e30b6cfe9fcf02e255dec22ba1
tanvir <tanvir.alam.shawn@gmail.com>
2018-06-10 02:39:20 +0200

    How to interact with diffrent elements of each DOM

--- test/counter.spec.js
+++ test/counter.spec.js
@@ -17,10 +17,26 @@ describe('Counter', () => {
     });
 
     //testing the underline logic
-    it('increments the count when the button is clicked', () => {
+    it('increments the count when the increment button is clicked', () => {
         expect(wrapper.vm.count).toBe(0);
-        wrapper.find('button').trigger('click');
+        wrapper.find('.increment').trigger('click');
         expect(wrapper.vm.count).toBe(1);
+    });
+
+    it('decrements the count when the decrement button is clicked', () => {
+        expect(wrapper.vm.count).toBe(0);
+        wrapper.find('.increment').trigger('click');
+        wrapper.find('.increment').trigger('click');
+        wrapper.find('.decrement').trigger('click');
+        expect(wrapper.vm.count).toBe(1);
+    });
+
+    it('never goes below 0', () => {
+        expect(wrapper.vm.count).toBe(0);
+        expect(wrapper.find('.decrement').hasStyle('display', 'none')).toBe(true);
+        wrapper.find('.increment').trigger('click');
+        expect(wrapper.find('.decrement').hasStyle('display', 'none')).toBe(false);
+        wrapper.find('.decrement').trigger('click');
     });
 
     //testing what is presented to the user
